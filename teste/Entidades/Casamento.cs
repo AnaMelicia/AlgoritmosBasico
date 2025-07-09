6c4b5852be926699768c514c3dc1ecbe8949552d
João Vitor <victor.flop@gmail.com>
2025-07-08 22:34:49 -0300

    Exercicios de classes

---
+++ teste/Entidades/Casamento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste.Entidades
+{
+    public class Casamento
+    {
+        public Casamento(string noiva, string noivo, string data, string local)
+        {
+            Noiva = noiva;
+            Noivo = noivo;
+            Data = data;
+            Local = local;
+        }
+        public Casamento()
+        {
+        }
+
+        public string Noiva { get;  private set; }
+        public string Noivo { get; private set; }
+        public string Data { get; private set; }
+        public string Local { get; private set; }
+
+        public void SetNoiva(string noiva)
+        {
+
+            Noiva = noiva;
+
+        }
+        public void SetNoivo(string noivo)
+        {
+
+            Noivo = noivo;
+
+        }
+        public void SetData(string data)
+        {
+
+            Data = data;
+
+        }
+        public void SetLocal(string local)
+        {
+
+            Local = local;
+
+        }
+
+        public void GerarConvite()
+        {
+            Console.WriteLine(Noiva + " e " + Noivo + " convidam você para seu casamento em " + Local + ", no dia " + Data +"!");
+        }
+
+    }
+}
--- teste/Program.cs
+++ teste/Program.cs
@@ -4,4 +4,4 @@ using teste.Entidades;
 //var ex = new MembroBTS();
 //ex.ListarMembros();
 
-Exercicio19.Executar();
+Exercicio21.Executar();
